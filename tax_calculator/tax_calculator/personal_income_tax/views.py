0f1a64dd99cf8e3bc7066413eb3542cdad85b484
maskudo <manujkarki101@gmail.com>
2023-02-27 17:46:26 +0545

    init template

--- tax_calculator/tax_calculator/personal_income_tax/views.py
+++ tax_calculator/tax_calculator/personal_income_tax/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello wold")
+    return render(request, "personal_income_tax/index.html", {})
