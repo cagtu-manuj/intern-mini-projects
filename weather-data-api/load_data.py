639055be22478e46c1edc82e6bbc7bbc232cf2c3
maskudo <manujkarki101@gmail.com>
2023-03-16 01:08:07 +0545

    load data from all files onto db and log time

--- weather-data-api/load_data.py
+++ weather-data-api/load_data.py
@@ -1,35 +1,44 @@
 import psycopg2
 import os
-from datetime import datetime
+import time
 
 # read from files
-# for filename in os.listdir(os.path.join(os.getcwd(), "wx_data")):
-# with open(os.path.join(os.getcwd(), filename), 'r') as f: # open in readonly mode
 
 conn = psycopg2.connect(host="localhost", dbname="postgres",
                         user="mk", password="acer1234", port=5432)
 
 cur = conn.cursor()
 
-with open(os.path.join(os.getcwd(), "wx_data", "USC00339312.txt"), "r") as file:
-    table_name = str(file.name.split('/')[-1].split('.')[0])
-    cur.execute("DROP TABLE IF EXISTS %s" % table_name)
-    sql = cur.mogrify("""CREATE TABLE IF NOT EXISTS %s(
-        date INT NOT NULL,
-        tmin INT,
-        tmax INT,
-        rain INT
-        );""" % (table_name))
-    cur.execute(sql)
-    for line in file.readlines():
-        data = line.split("\t")
-        data = [int(item.strip()) for item in data]
-        data.insert(0, table_name.lstrip())
-        data = tuple(data)
-        sql = "Insert INTO %s (date, tmin, tmax, rain) VALUES (%s, %s , %s, %s);" % data
+start_time = time.time()
+total_records = 0
+total_tables = 0
+for filename in os.listdir(os.path.join(os.getcwd(), "wx_data")):
+    total_tables += 1
+    with open(os.path.join(os.getcwd(), "wx_data",  filename), 'r') as file:
+        table_name = str(file.name.split('/')[-1].split('.')[0])
+        cur.execute("DROP TABLE IF EXISTS %s" % table_name)
+        sql = cur.mogrify("""CREATE TABLE IF NOT EXISTS %s(
+            date INT NOT NULL,
+            tmin INT,
+            tmax INT,
+            rain INT
+            );""" % (table_name))
         cur.execute(sql)
-
+        for line in file.readlines():
+            data = line.split("\t")
+            data = [int(item.strip()) for item in data]
+            data.insert(0, table_name.lstrip())
+            data = tuple(data)
+            # worry about sql injection later
+            sql = "Insert INTO %s (date, tmin, tmax, rain) VALUES (%s, %s , %s, %s);" % data
+            # execute many
+            cur.execute(sql)
+            total_records += 1
 
 conn.commit()
 cur.close()
 conn.close()
+end_time = time.time()
+print(f"Total tables added: {total_tables}")
+print(f"Total rows added: {total_records}")
+print(f"Total time: {end_time - start_time} ns")
