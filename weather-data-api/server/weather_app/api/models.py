844db539a65f534425ff1ceca8836e23aa072bf2
maskudo <manujkarki101@gmail.com>
2023-03-20 16:49:49 +0545

    change model and load data from all files in wx_data

--- weather-data-api/server/weather_app/api/models.py
+++ weather-data-api/server/weather_app/api/models.py
@@ -3,10 +3,14 @@ from django.db import models
 
 # datetime.date(1997, 10, 19
 class Weather(models.Model):
-    date = models.DateField(unique=True, null=False)
+    station = models.CharField(max_length=15)
+    date = models.DateField(null=False)
     tmin = models.IntegerField()
     tmax = models.IntegerField()
     rain = models.IntegerField()
+
+    class Meta:
+        ["station", "date"]
 
 
 class Summary(models.Model):
--- weather-data-api/server/weather_app/scripts/load_database.py
+++ weather-data-api/server/weather_app/scripts/load_database.py
@@ -2,20 +2,34 @@ import pandas as pd
 import datetime
 from api.models import Weather
 import os
+import time
 
 
 def run():
-    df = pd.read_csv("USC00110072.txt", sep="\t", header=1, parse_dates=True)
-    df.columns = ["date", "tmin", "tmax", "rain"]
-    df["date"] = pd.to_datetime(df.date, format="%Y%m%d")
-    df_records = df.to_dict("records")
-    model_instances = [
-        Weather(
-            date=record["date"],
-            tmin=record["tmin"],
-            tmax=record["tmax"],
-            rain=record["rain"],
-        )
-        for record in df_records
-    ]
-    Weather.objects.bulk_create(model_instances)
+    total_records = 0
+    total_tables = 0
+    start_time = time.time()
+    for filename in os.listdir(os.path.join(os.getcwd(), "wx_data")):
+        total_tables += 1
+        with open(os.path.join(os.getcwd(), "wx_data", filename), "r") as file:
+            station = str(file.name.split("/")[-1].split(".")[0])
+            df = pd.read_csv("USC00110072.txt", sep="\t", header=1, parse_dates=True)
+            df.columns = ["date", "tmin", "tmax", "rain"]
+            df["date"] = pd.to_datetime(df.date, format="%Y%m%d")
+            df_records = df.to_dict("records")
+            model_instances = [
+                Weather(
+                    station=station,
+                    date=record["date"],
+                    tmin=record["tmin"],
+                    tmax=record["tmax"],
+                    rain=record["rain"],
+                )
+                for record in df_records
+            ]
+            Weather.objects.bulk_create(model_instances)
+
+    end_time = time.time()
+    print(f"Total tables added: {total_tables}")
+    # print(f"Total rows added: {total_records}")
+    print(f"Total time: {end_time - start_time}")
