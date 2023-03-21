7565ffa1a7a90951670094aee35dc52b2e526e45
maskudo <manujkarki101@gmail.com>
2023-03-21 17:57:52 +0545

    setup initial path

--- weather-data-api/server/weather_app/api/urls.py
+++ weather-data-api/server/weather_app/api/urls.py
@@ -1,7 +1,7 @@
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getData),
+    path("api/weather", views.getWeatherData),
+    path("api/weather/stats", views.getWeatherStats),
 ]
